2d3cbe34557395bfe0e21e06480a452f51516aec
Pujan-Ajmera <160005231+Pujan-Ajmera@users.noreply.github.com>
2025-08-01 20:12:15 +0530

    Add files via upload

---
+++ four/lib/app/controllers/db_controller.dart
@@ -0,0 +1,30 @@
+import 'package:get/get.dart';
+import '../../utils/import_export.dart';
+class DBController extends GetxController {
+  final DBService dbService = Get.find<DBService>(); // Use GetX DI
+  var categories = <Category>[].obs;
+  var concepts = <Concept>[].obs;
+
+  @override
+  void onInit() {
+    initDB();
+    super.onInit();
+  }
+
+  Future<void> initDB() async {
+    await dbService.initDatabase();
+    await fetchCategories();
+  }
+
+  Future<void> fetchCategories() async {
+    final db = await dbService.database;
+    final data = await db.query('Category');
+    categories.value = data.map((e) => Category.fromMap(e)).toList();
+  }
+
+  Future<void> fetchConcepts(int categoryId) async {
+    final db = await dbService.database;
+    final data = await db.query('Concepts', where: 'categoryId = ?', whereArgs: [categoryId]);
+    concepts.value = data.map((e) => Concept.fromMap(e)).toList();
+  }
+}
